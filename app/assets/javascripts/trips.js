ffbff3013f01a1e3dddfbe423b9ac220d533d324
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-30 21:17:47 +0000

    adding comments views and form

--- app/assets/javascripts/trips.js
+++ app/assets/javascripts/trips.js
@@ -7,8 +7,7 @@ $(function() {
          //Get the response (it's the variable data)
         $("div.comments").html(response)
          //We'd really want to load that data into the DOM (add it to the current page)
-      }).error(function(notNeeded){
-        alert("we broke!!!!")
-      });
-  }
+      })
+    e.preventDefault();
+  })
 })
