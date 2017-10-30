0c2139c60aed76d0b6726d7ebf2d804ca90ade1a
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-30 21:28:49 +0000

    requesting comments with json

--- app/assets/javascripts/trips.js
+++ app/assets/javascripts/trips.js
@@ -1,13 +1,24 @@
 $(function() {
   $("a.load_comments").on("click", function(e){
-    $.ajax({
-        method: "GET",
-        url: this.href
-      }).success(function(response){
+  //  $.ajax({
+  //      method: "GET",
+  //      url: this.href
+  //    }).success(function(response){
          //Get the response (it's the variable data)
-        $("div.comments").html(response)
+  //      $("div.comments").html(response)
          //We'd really want to load that data into the DOM (add it to the current page)
-      })
-    e.preventDefault();
+    //  })
+  //  e.preventDefault();
+//  })
+
+// Requesting JSON
+ $.get(this.href).success(function(json){
+   var $ol = $("div.comments ol")
+   $ol.html("")
+   json.forEach(function(comment){
+     $ol.append("<li>" + comment.content + "</li>");
+   })
   })
+  e.preventDefault();
+})
 })
