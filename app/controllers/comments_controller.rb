2405565e6517c91eeadca90131b8aaf7a6bf6356
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-31 01:27:23 +0000

    adding to trips show

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,25 +2,31 @@ class CommentsController < ApplicationController
   before_action :set_trip
 
   def index
-    @comments = @trip.comments
-
-    respond_to do |format|
-      format.html {render 'index.html', :layout => false}
-      format.js {render 'index.js', :layout => false}
-    end
+    @comments = Comment.all
+    render json: @comments
   end
+
+  #def index
+  #  @comments = @trip.comments
+  #  respond_to do |format|
+  #    format.html {render 'index.html', :layout => false}
+  #    format.js {render 'index.js', :layout => false}
+  #  end
+  #end
 
   def create
-    @comment = @trip.comments.build(comments_params)
-    if @comment.save
-      # I need to render something that just has the LI I want...
-      # why not just create a comments/show view that shows the LI of one comment?
-      # render 'comments/show', :layout => false
-      redirect_to @trip
-    else
-      render "trips/show"
-    end
+    @comment = Comment.create(comments_params)
+    render json: @comment, status: 201
   end
+
+  #def create
+  #  @comment = @trip.comments.build(comments_params)
+  #  if @comment.save
+  #    redirect_to @trip
+  #  else
+  #    render "trips/show"
+  #  end
+  #end
 
  def destroy
    @comment = Comment.find(params[:id])
@@ -35,6 +41,6 @@ class CommentsController < ApplicationController
     end
 
     def comments_params
-      params.require(:comment).permit(:content, :trip_id, :user_id)
+      params.require(:comment).permit(:id, :content, :trip_id, :user_id)
     end
 end
